1ebfe0e2a8810cbf59940f3c1fae96add73bb8a1
Iyana Medlock <iyanamedlock@gmail.com>
2021-04-03 21:38:08 -0500

    desktop ui finished

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,37 @@
 Splitting();
 
-$(".nav-link").on("click", function(e) {
+$(".nav-link").on("click", function() {
     $(".nav-link").removeClass("active");
     $(this).addClass("active", 300);
-    e.preventDefault();
+    
   });
+
+  let index = 1;
+  showSlide(index);
+
+  function plus(n) {
+    showSlide((index += n));
+  }
+  $(".prev").on("click", function () {
+    plus(-1);
+  });
+  $(".next").on("click", function () {
+    plus(1);
+  });
+  function showSlide(n) {
+    let slides = $(".slides");
+
+    if (n > slides.length) {
+      index = 1;
+    }
+
+    if (n < 1) {
+      index = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+      $(slides[i]).css("display", "none");
+    }
+
+    $(slides[index - 1]).css("display", "block");
+  }
